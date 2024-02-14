6077cbe5bc0e6c38d7fdc93d127247ca434f3b54
ksadiki <sadikikhalil9@gmail.com>
2024-02-14 16:40:20 +0100

    isnegative.go

---
+++ isnegative.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func IsNegative(nb int) {
+	if nb < 0 {
+		z01.PrintRune('T')
+	} else {
+		z01.PrintRune('F')
+	}
+	z01.PrintRune('\n')
+}
+
+func main() {
+	IsNegative(-3)
+}
