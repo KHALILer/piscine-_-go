df5df8b7d05ef8c81d42a203fb42bbd2c83035c3
Christopher  Fremond <dev@01-edu.org>
2024-02-15 17:34:39 +0000

    Add 'printcomb.go'

---
+++ printcomb.go
@@ -0,0 +1,15 @@
+package piscine 
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for a := '0' ; a <= '9' ; a++ {
+		for b := '0' ; b <= '8' ; b++ {
+			for c := '0' ; c <= '9' ; c++ {
+				for d := '9' ; d <= '9' ; d++ {
+				
+			}
+		}
+	}
+  z01.PrintRune('\n')
+}
