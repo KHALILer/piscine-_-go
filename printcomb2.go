d611ab533b000c820916c0d30a85dd9013a6990d
ksadiki <sadikikhalil9@gmail.com>
2024-02-15 18:21:43 +0100

    printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -1,10 +1,10 @@
-package main
+package piscine
 
 import (
 	"github.com/01-edu/z01"
 )
 
-func main() {
+func PrintComb2() {
 	for a := '0'; a <= '9'; a++ {
 		for b := '0'; b <= '8'; b++ {
 			for c := '0'; c <= '9'; c++ {
