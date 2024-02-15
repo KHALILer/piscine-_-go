864c3ca21ecc11e2818f880774c57949dd111cef
ksadiki <sadikikhalil9@gmail.com>
2024-02-15 17:50:22 +0100

    printcomb2.go

---
+++ printcomb2.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '8'; b++ {
+			for c := '0'; c <= '9'; c++ {
+				for d := '1'; d <= '9'; d++ {
+					if a < c || b < d && a == c {
+						if a != '9' || b != '9' {
+							z01.PrintRune(a)
+							z01.PrintRune(b)
+							z01.PrintRune(' ')
+							z01.PrintRune(c)
+							z01.PrintRune(d)
+							if a == '9' && b == '8' && c == '9' && d == '9' {
+								break
+							}
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
