7b74c2af2dc1783a904eafb0335c1028f7734417
ksadiki <sadikikhalil9@gmail.com>
2024-02-20 23:55:29 +0100

    iterativepower2

--- iterativepower.go
+++ iterativepower.go
@@ -8,10 +8,10 @@ func IterativePower(nb int, power int) int {
 	if power == 0 {
 		return 1
 	} else {
-		n := 1
+
 		for i := 1; i <= power; i++ {
-			n *= nb
+			nb *= nb
 		}
-		return n
+		return nb
 	}
 }
