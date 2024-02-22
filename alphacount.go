7942e3324fa46760313bc471b1b111bd830ad4f0
ksadiki <sadikikhalil9@gmail.com>
2024-02-22 15:37:19 +0100

    alphacount.go

--- alphacount.go
+++ alphacount.go
@@ -1,13 +1,11 @@
 package piscine
 
 func AlphaCount(s string) int {
-
-    cnt := 0
-    for _, char := range s {
-        if char >= 'A' && char <= 'Z' || char >= 'a' && char <= 'z' {
-            cnt++
-        }
-    }
-    return cnt
-} 
-
+	cnt := 0
+	for _, char := range s {
+		if char >= 'A' && char <= 'Z' || char >= 'a' && char <= 'z' {
+			cnt++
+		}
+	}
+	return cnt
+}
