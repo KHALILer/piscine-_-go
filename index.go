9dda43dcbcd1b89dcd25c01dc65b4c88b78f9380
ksadiki <sadikikhalil9@gmail.com>
2024-02-23 10:56:50 +0100

    index

---
+++ index.go
@@ -0,0 +1,32 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	if len(s) < len(toFind) {
+		return -1
+	}
+	index := 0
+	if len(toFind) == 1 {
+		for i := 0; i < len(s); i++ {
+			if s[i] == toFind[0] {
+				index = i
+				break
+			}
+		}
+	}
+	if len(toFind) > 1 {
+		for i := 0; i < len(s); i++ {
+			if s[i] == toFind[0] {
+				index = i
+			}
+		}
+		c := index
+		for j := 0; j < len(toFind); j++ {
+			if s[c] == toFind[j] {
+				c++
+			} else {
+				index = -1
+			}
+		}
+	}
+	return index
+}
