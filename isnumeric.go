a561bcadff48470522ead1bdef6933a6f5d44e74
ksadiki <sadikikhalil9@gmail.com>
2024-02-23 12:12:37 +0100

    isnumeric

---
+++ isnumeric.go
@@ -0,0 +1,14 @@
+package package
+
+func IsNumeric(s string) bool {
+	rn := []rune(s)
+	f := false
+	for i := range rn {
+		if rn[i] >= '0' && rn[i] <= '9' {
+			f = true
+		} else {
+			return false
+		}
+	}
+	return f
+}
