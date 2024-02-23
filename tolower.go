d7ff289a66707ff7af49890e8851c82753f1b6d0
ksadiki <sadikikhalil9@gmail.com>
2024-02-23 13:29:38 +0100

    tolower

---
+++ tolower.go
@@ -0,0 +1,11 @@
+package main
+
+func ToLower(s string) string {
+	rn := []rune(s)
+	for i := 0; i < len(s); i++ {
+		if rn[i] >= 'A' && rn[i] <= 'Z' {
+			rn[i] += 32
+		}
+	}
+	return string(rn)
+}
