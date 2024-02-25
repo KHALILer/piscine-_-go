ca0e56b0643a632cd974c7e6a03013ca521ceae9
ksadiki <sadikikhalil9@gmail.com>
2024-02-25 18:13:05 +0100

    appendrange

--- appendrange.go
+++ appendrange.go
@@ -2,7 +2,7 @@ package piscine
 
 func AppendRange(min, max int) []int {
 	array := []int{}
-	for i := min - 1; i < max-1; i++ {
+	for i := min; i < max; i++ {
 		array = append(array, i+1)
 	}
 	return array
