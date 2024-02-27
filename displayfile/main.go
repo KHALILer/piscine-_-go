41e9695560cfdb915fd2f6761fa997ce99df5dab
ksadiki <sadikikhalil9@gmail.com>
2024-02-27 16:01:57 +0100

    displayfile

---
+++ displayfile/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	arguments := os.Args[1:]
+	length := 0
+	for i := range arguments {
+		length = i + 1
+	}
+
+	if length > 1 {
+		fmt.Print("Too many arguments")
+	} else if length == 0 {
+		fmt.Print("File name missing")
+	} else {
+
+		content, _ := ioutil.ReadFile(arguments[0])
+
+		fmt.Println(string(content))
+
+	}
+}
