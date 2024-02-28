e55384aa074a5f4a4a5aa0b847264d1b6cbebb69
ksadiki <sadikikhalil9@gmail.com>
2024-02-28 17:59:44 +0100

    issorted01

--- issorted.go
+++ issorted.go
@@ -1,12 +1,19 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	if len(a) <= 1 {
-		return true
-	}
 	for i := 1; i < len(a); i++ {
 		if f(a[i-1], a[i]) > 0 {
-			return false
+			for i := 2; i < len(a); i++ {
+				if f(a[i-1], a[i]) < 0 {
+					return false
+				}
+			}
+		} else if f(a[i-1], a[i]) < 0 {
+			for i := 2; i < len(a); i++ {
+				if f(a[i-1], a[i]) > 0 {
+					return false
+				}
+			}
 		}
 	}
 	return true
