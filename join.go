42524f719e8f62f7594bfefd0e24f0fd9f7ede97
ksadiki <sadikikhalil9@gmail.com>
2024-02-29 21:48:07 +0100

    join

--- join.go
+++ join.go
@@ -1,11 +1,12 @@
 package piscine
 
-func IsSorted(f func(a, b int) int, a []int) bool {
-	found := true
-	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) < 0 {
-			found = false
+func Join(strs []string, sep string) string {
+	s := ""
+	for i, word := range strs {
+		s += word
+		if i != len(strs)-1 {
+			s += sep
 		}
 	}
-	return found
+	return s
 }
