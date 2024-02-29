f2c1143aca4d2be36f4216f2b9884c6b55e8f045
ksadiki <sadikikhalil9@gmail.com>
2024-02-29 11:58:03 +0100

    descendcomb

--- descendcomb.go
+++ descendcomb.go
@@ -9,9 +9,6 @@ func DescendComb() {
 		for j := '9'; j >= '0'; j-- {
 			for k := '9'; k >= '0'; k-- {
 				for l := '9'; l >= '0'; l-- {
-					if i == '0' && j == '0' || k >= j || l >= j {
-						continue
-					}
 					if (i > k) || (i == k && j > 1) {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
