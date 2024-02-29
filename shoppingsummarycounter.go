4834235a79b2f8a8babea43f8ff3742a55f13672
ksadiki <sadikikhalil9@gmail.com>
2024-02-29 16:51:07 +0100

    shoppingsummarycounter

---
+++ shoppingsummarycounter.go
@@ -0,0 +1,18 @@
+package piscine
+
+func ShoppingSummaryCounter(str string) map[string]int {
+	Map := make(map[string]int)
+	item := ""
+	for _, words := range str {
+		if words != ' ' {
+			item += string(words)
+		} else {
+			if item != "" {
+				Map[item]++
+				item = ""
+			}
+		}
+	}
+	Map[item]++
+	return Map
+}
