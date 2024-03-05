12c6fee04be0b5e0da7bb0e3ebb8103cd75736f0
ksadiki <sadikikhalil9@gmail.com>
2024-03-05 13:11:08 +0100

    listpushback

---
+++ listpushback.go
@@ -0,0 +1,23 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	newNode := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = newNode
+		l.Tail = newNode
+		return
+	}
+	l.Tail.Next = newNode
+	l.Tail = newNode
+
+}
