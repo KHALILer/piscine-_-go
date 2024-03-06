72c2d38c5df12a97601ebaced8ae37cd510b5380
ksadiki <sadikikhalil9@gmail.com>
2024-03-06 16:53:04 +0100

    list

--- listpushback.go
+++ listpushback.go
@@ -1,15 +1,5 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 func ListPushBack(l *List, data interface{}) {
 	newNode := &NodeL{Data: data}
 	if l.Head == nil {
--- listsize.go
+++ listsize.go
@@ -1,15 +1,5 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 func ListSize(l *List) int {
 	count := 0
 	for l.Head != nil {
