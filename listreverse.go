d577942b136fad75d913d00403dac7b94c9d9fda
ksadiki <sadikikhalil9@gmail.com>
2024-03-07 14:40:34 +0100

    lists

--- listreverse.go
+++ listreverse.go
@@ -1,15 +1,5 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 func ListReverse(l *List) {
 	var prev *NodeL
 	current := l.Head
