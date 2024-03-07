fca820896d40e5493ab0e22c279c932a9b3fd009
ksadiki <sadikikhalil9@gmail.com>
2024-03-07 15:10:36 +0100

    lists

--- listforeach.go
+++ listforeach.go
@@ -1,15 +1,5 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 func ListForEach(l *List, f func(*NodeL)) {
 	node := l.Head
 	for node != nil {
