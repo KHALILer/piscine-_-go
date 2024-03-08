a8c3e6aa4f508f9b6d7c00868e1afa773be4985e
ksadiki <sadikikhalil9@gmail.com>
2024-03-08 12:07:25 +0100

    btreeapplyinorder

---
+++ btreeapplyinorder.go
@@ -0,0 +1,11 @@
+package piscine
+
+func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
+
+	if root != nil {
+		BTreeApplyInorder(root.Left, f)
+		f(root.Data)
+		BTreeApplyInorder(root.Right, f)
+	}
+
+}
