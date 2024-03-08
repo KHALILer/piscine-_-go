bb44571f6e6fed5a1f756e523a3c8a5b49a8e719
ksadiki <sadikikhalil9@gmail.com>
2024-03-08 11:09:05 +0100

    lists

--- listforeachif.go
+++ listforeachif.go
@@ -1,15 +1,5 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 func IsPositiveNode(node *NodeL) bool {
 	switch node.Data.(type) {
 	case int, float32, float64, byte:
---
+++ tst/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	link := &piscine.List{}
+
+	piscine.ListPushBack(link, "hello")
+	piscine.ListPushBack(link, "hello1")
+	piscine.ListPushBack(link, "hello2")
+	piscine.ListPushBack(link, "hello3")
+
+	found := piscine.ListFind(link, interface{}("hello2"), piscine.CompStr)
+
+	fmt.Println(found)
+	fmt.Println(*found)
+}
